dab5d54904513250ce20418328b7404cd16bc8a4
ovidiu <ovidiu.marus@stud.acs.upb.ro>
2016-12-12 21:31:25 +0200

    added a function that filters the words in a dictionary

---
+++ filterDictionaryWords.py
@@ -0,0 +1,21 @@
+# function that deletes non-corresponding words (such as names) from the
+# dictionary
+
+def filterDictionaryWords ( ):
+    fileName = "tempDictionary"
+    filteredDict = "dictionary"
+    inputFile = open(fileName, "r")         #open the dictionary file
+    outputFile = open(filteredDict, "w")
+
+
+    for tempTuple in enumerate(inputFile):
+        line = tempTuple[1]
+        if (str(line[0]).isupper() is False and '\'' not in line
+                                            and len(line) > 2):
+            outputFile.write(str(line))
+
+
+    inputFile.close()       #closing the input file
+    outputFile.close()      #closing the output file
+
+filterDictionaryWords()
