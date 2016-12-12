cea8ba128f2745a1f86601233d11e3cbb525466f
ovidiu <ovidiu.marus@stud.acs.upb.ro>
2016-12-12 21:59:35 +0200

    added a function that gets the standard current os dictionary

---
+++ getDictionary.py
@@ -0,0 +1,14 @@
+import os
+import platform
+
+def getDictionary( ):
+    from shutil import copyfile
+
+    if (platform.system() == 'Linux'):
+        copyfile(os.path.realpath
+        ('/usr/share/dict/words.pre-dictionaries-common'), './dictionary')
+    else:
+         if (platform.system() == 'Windows'):
+             pass #TODO: get dictionary from windows
+    #else:
+        #TODO: unsupported operating system action
