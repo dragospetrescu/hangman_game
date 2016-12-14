1b6c11dd1b956506a5a8f6047aeec0573538c499
Dragos <dragos.petrescu@stud.acs.upb.ro>
2016-12-14 21:40:58 +0200

    added new game button

--- hangManGUI.py
+++ hangManGUI.py
@@ -17,6 +17,9 @@ def onKeyPress(event):
     if ok == 0:
         wrong_letters.append (letter)
 
+def newgame():
+    global global_ok
+    global_ok = 1
 
 
 # create the dictionary file
@@ -24,51 +27,66 @@ if not os.path.isfile("dictionary"):
     getDictionary ()
     filterDictionaryWords()
 
-
-# word recieves a random word from the computers dictionary
-# found is the file
-word = getRandomWord()
-found = list (list ('_')*len(word))
-
-# make the first and last letter known and all their repetitions
-for i in range (0, len(word)):
-    if word [i] is word [0]:
-        found [i] = word [0]
-    elif word [i] is word [-1]:
-        found [i] = word [-1]
-
-wrong_letters =[]
-error = 0
-
 # create window and define events
 root = Tk()
 root.geometry('1000x1000')
+global_ok = 1
 
-label_err = Label (root, text ="Number of errors: "+ str(error))
-label_err.config(font=("Courier", 20))
-label_err.place(anchor="e")
-label_err.pack()
+while global_ok == 1:
+    global_ok = 0
 
-label_wrong = Label (root, text ="Errors: "+ str(wrong_letters))
-label_wrong.config(font=("Courier", 20))
-label_wrong.place(anchor="e")
-label_wrong.pack()
+    # word recieves a random word from the computers dictionary
+    # found is the file
+    word = getRandomWord()
+    found = list (list ('_')*len(word))
+
+    # make the first and last letter known and all their repetitions
+    for i in range (0, len(word)):
+        if word [i] is word [0]:
+            found [i] = word [0]
+        elif word [i] is word [-1]:
+            found [i] = word [-1]
+
+    wrong_letters =[]
+    error = 0
 
 
-label_found = Label (root, text = found)
-label_found.config(font=("Courier", 44))
-label_found.place(anchor="center")
-label_found.pack()
-root.bind('<KeyPress>', onKeyPress)
-while found != word and error < 5:
-    label_found.config(text=found)
-    label_wrong.config(text ="Errors: "+ str(wrong_letters))
-    error = len(wrong_letters)
-    label_err.config(text ="Number of errors: "+ str(error))
+
+    label_err = Label (root, text ="Number of errors: "+ str(error))
+    label_err.config(font=("Courier", 20))
+    label_err.place(anchor="e")
+    label_err.pack()
+
+    label_wrong = Label (root, text ="Errors: "+ str(wrong_letters))
+    label_wrong.config(font=("Courier", 20))
+    label_wrong.place(anchor="e")
+    label_wrong.pack()
+
+
+    label_found = Label (root, text = found)
+    label_found.config(font=("Courier", 44))
+    label_found.place(anchor="center")
+    label_found.pack()
+    root.bind('<KeyPress>', onKeyPress)
+    while found != word and error < 5:
+        label_found.config(text=found)
+        label_wrong.config(text ="Errors: "+ str(wrong_letters))
+        error = len(wrong_letters)
+        label_err.config(text ="Number of errors: "+ str(error))
+        root.update()
+    if found == word:
+        label_wrong.config(text ="Congratulations, you did it!")
+        label_found.config(text=word)
+    else:
+        label_wrong.config(text ="You lost! The word was:")
+        label_found.config(text=word)
+    btt_new_game= Button(root, text ="New Game", command = newgame)
+    btt_new_game.pack()
+
+    while global_ok == 0:
+        root.update()
+    label_found.pack_forget()
+    label_wrong.pack_forget()
+    label_err.pack_forget()
+    btt_new_game.pack_forget();
     root.update()
-if found == word:
-    label_wrong.config(text ="Congratulations, you did it!")
-else:
-    label_wrong.config(text ="You lost! The word was:")
-root.mainloop();
-# read a letter and check if word contains that letter and makes is known
